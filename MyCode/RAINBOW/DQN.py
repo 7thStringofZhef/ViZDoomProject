989302f2de05b3b228743c7d75a1c3f9418327b3
Unknown <david.slayback2010@gmail.com>
2018-11-30 13:28:46 -0500

    More updates

--- MyCode/RAINBOW/DQN.py
+++ MyCode/RAINBOW/DQN.py
@@ -5,36 +5,23 @@ import torch
 import numpy as np
 
 #Basic DQN architecture
-class DQN(nn.Module):
-    def __init__(self, inputShape, numActions):
-        super(DQN, self).__init__()
-        self.numActions = numActions
-        self.inputShape = inputShape
-        # Convolutional layers
-        self.conv = nn.Sequential(
-            nn.Conv2d(inputShape[0], 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU()
-        )
-        convOutputSize = self.getConv(inputShape)
+class DQN(object):
+    def __init__(self, screenShape, historySize):
+        self.screenShape = screenShape
+        self.histSize = historySize
 
-        # Fully connected layers
-        self.fc = nn.Sequential(
-            nn.Linear(convOutputSize, 512),
-            nn.RelU(),
-            nn.Linear(512, numActions)
-        )
 
-    # Get the output size of the convolutional layer
-    def getConvOutputSize(self, shape):
-        output = self.conv(V(torch.zeros(1, *shape)))
-        return int(np.prod(output.size()))
+def buildCNNComponent(module, inputShape):
+    channels, height, width = inputShape
+    module.convolutional = nn.Sequential([
+        nn.Conv2d(channels, 32, (8, 8), stride=(4, 4)),
+        nn.BatchNorm2d(32),
+        nn.ReLU(),
+        nn.Conv2d(32, 64, (4,4), stride=(2,2)),
+        nn.BatchNorm2d(64),
+        nn.ReLU,
+    ])
+    x = torch.autograd.Variable(torch.FloatTensor(1, height, width))
+    module.convolutionalOutputDim = module.convolutional(x).nelement()
 
-    # Run input through the network, return Q values of actions
-    def forward(self, input):
-        rescaledInput = input.float() / 256
-        convOutput = self.conv(rescaledInput).view(rescaledInput.size()[0], -1)
-        return self.fc(convOutput)
+def buildRecurrentLayer(module, inputShape)
