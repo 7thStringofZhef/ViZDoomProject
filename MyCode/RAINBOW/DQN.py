72c6919b096b7889907c79e7bf6783e52580a73c
Unknown <david.slayback2010@gmail.com>
2018-11-30 17:52:22 -0500

    And more

--- MyCode/RAINBOW/DQN.py
+++ MyCode/RAINBOW/DQN.py
@@ -5,12 +5,17 @@ import torch
 import numpy as np
 
 #Basic DQN architecture
-class DQN(object):
-    def __init__(self, screenShape, historySize):
-        self.screenShape = screenShape
-        self.histSize = historySize
+class DQNModule(nn.Module):
+    def __init__(self, inputShape=(3,60,108), gameVars=[('health', 101), ('AMMO2', 301)], gameVarBucketSizes=[10,1]):
+        super(DQNModule, self).__init__()
+
+        buildCNNComponent(self, inputShape)
+        self.outputDim = self.convolutionalOutputDim
+
+        buildGameVariableComponent(self, gameVars, gameVarBucketSizes)
 
 
+# CNN component of DQN (based on Arnold)
 def buildCNNComponent(module, inputShape):
     channels, height, width = inputShape
     module.convolutional = nn.Sequential([
@@ -24,4 +29,25 @@ def buildCNNComponent(module, inputShape):
     x = torch.autograd.Variable(torch.FloatTensor(1, height, width))
     module.convolutionalOutputDim = module.convolutional(x).nelement()
 
-def buildRecurrentLayer(module, inputShape)
+# Game variables component of DQN (e.g., health and ammo)
+class GameVariableEmbedding(nn.Embedding):
+    def __init__(self, bucketSize, numEmbeddings, *args, **kwargs):
+        self.bucketSize = bucketSize
+        trueNumEmbeddings = (numEmbeddings+bucketSize-1) // bucketSize
+        super(GameVariableEmbedding, self).__init__(trueNumEmbeddings, *args, **kwargs)
+
+    def forward(self, indices):
+        return super(GameVariableEmbedding, self).forward(indices.div(self.bucketSize))
+
+def buildGameVariableComponent(module, gameVars, gameVarBucketSizes):
+    module.gameVariables = gameVars
+    module.numGameVariables = len(gameVars)
+    module.gameVariableEmbeddings = []
+    for i, (name, numValues) in enumerate(gameVars):
+        embedding = GameVariableEmbedding(gameVarBucketSizes[i], numValues)
+        setattr(module, '%s_emb' % name, embedding)
+        module.gameVariableEmbeddings.append(embedding)
+
+
+def buildRecurrentLayer(module, inputShape):
+    module = nn.LSTM(512)
