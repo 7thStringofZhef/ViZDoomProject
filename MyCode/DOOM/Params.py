f3e1732c36802c0866c72b5e03b9d7e9f6c338b9
Unknown <david.slayback2010@gmail.com>
2018-12-01 16:09:36 -0500

    Changed my mind on configuration file type

--- MyCode/DOOM/Params.py
+++ MyCode/DOOM/Params.py
@@ -1,8 +1,43 @@
-DOOM_PARAMS = {
-    'gamma': 0.99,
-    'alpha': 0.1,
-    'startEps': 1,
-    'endEps': 0.02,
-    'batchSize': 64,
-    'netInputShape': (320, 240)
-}
+# Enormous parameter class
+from vizdoom import GameVariable
+
+class doomParams(object):
+    #RAINBOW COMPONENTS
+    prioritizedReplay = 1
+    noisyLinear = 1
+    dueling = 1
+    double = 1
+    multiStep = 3
+
+    #[NET PARAMS]
+    startEps=1.0
+    endEps=0.1
+    epsSteps=100000
+    gamma=0.99
+    batchSize=32
+    frameskip=4
+    framesBeforeTraining=80000
+    learningRate=0.001
+    recurrenceHistory=4
+    trainingFrequency=4
+    minHistorySize=4
+    historySize=8
+    numRecurrentUpdates=2
+    embeddingDim=32
+
+    #[RAINBOW PARAMS]
+    targetUpdateFrequency=1000
+    noisyParam=0.5
+    priorityBetaStart=0.4
+    priorityBetaEnd=1
+    priorityBetaSteps=200000
+    priorityOmega=0.5
+
+    #Other
+    replayMemoryCapacity=500000
+    inputShape = (3, 60, 108)  # Channels*width*height
+    hiddenDimensions = 512
+    gameVariables=[GameVariable.AMMO2, GameVariable.HEALTH]
+    gameVariableBucketSizes=[1, 1]
+
+
--- MyCode/doomMain.py
+++ MyCode/doomMain.py
@@ -1,14 +1,17 @@
+from __future__ import absolute_import
+
 import configparser
 import torch
 from functools import partial
 import numpy as np
 
-from vizdoom import DoomGame, Mode, ScreenFormat, ScreenResolution
-from .DOOM.FrameProcessing import processImage
-from .RAINBOW.ModelMemory import PrioritizedReplayMemory, LinearSchedule
+from vizdoom import DoomGame, Mode, ScreenFormat, ScreenResolution, GameVariable
+from DOOM.FrameProcessing import processImage
+from RAINBOW.ModelMemory import PrioritizedReplayMemory, LinearSchedule
 
 def parseHyperparamIni(filepath='Hyperparams.ini'):
-    netConfig = configparser.ConfigParser(filepath)
+    netConfig = configparser.ConfigParser()
+    netConfig.read(filepath)
     rainbowUse = netConfig['RAINBOW COMPONENTS']
     rainbowParams = netConfig['RAINBOW PARAMS']
     netParams = netConfig['NET PARAMS']
@@ -43,10 +46,11 @@ class DoomGameEnv(object):
         self.actions = np.identity(self.numActions, dtype=np.int32).tolist()  # One-hot encoding of actions
 
     def step(self, action):
-        frameBuffer = torch.zeros(3, self.imageHeight, self.imageWidth)
         reward = 0
         is_done = False
-        reward = self.game.make_action(action, self.frameskip)  # Take action for frameskip frames
+        self.game.set_action(action)
+        for _ in range(self.frameskip):
+            reward += game.advance_action()
         is_done = self.game.is_episode_finished() or self.game.is_player_dead()
         newState = self.game.get_state()
         return newState, reward, is_done
@@ -65,8 +69,8 @@ if __name__ == "__main__":
 
     # Define basic network
 
-    betaSchedule = LinearSchedule(200000, 1, 0.4)
-    epsSchedule = LinearSchedule(100000, 0.1, 1)  # If using, eps decay
+    #betaSchedule = LinearSchedule(200000, 1, 0.4)
+    #epsSchedule = LinearSchedule(100000, 0.1, 1)  # If using, eps decay
 
     pass
 
